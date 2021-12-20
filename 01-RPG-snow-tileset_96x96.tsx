f6a955dd08a012b29ff6061305b4098b742ff200
Oliver-Marcus Eder <github@derkonzern.com>
2021-12-20 14:08:05 +0100

    Delete 01-RPG-snow-tileset_96x96.tsx

--- 01-RPG-snow-tileset_96x96.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="01-RPG-snow-tileset_96x96" tilewidth="32" tileheight="32" tilecount="100" columns="10">
- <image source="src/assets/01-RPG-snow-tileset_96x96.png" width="1024" height="1024"/>
-</tileset>
